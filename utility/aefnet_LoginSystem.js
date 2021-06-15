c3e80b2cd2ba5250f1f4a0bd46c2a435fd5ba25e
AEFNET <71930612+AlperenFural@users.noreply.github.com>
2021-06-15 16:37:54 +0200

    Update Veritabani

--- utility/aefnet_LoginSystem.js
+++ utility/aefnet_LoginSystem.js
@@ -1,8 +1,9 @@
 const Sequelize = require("sequelize");
 
-const aefnet_LoginSystem = new Sequelize("aefnet_LoginSystem","aefnet_Login","PPht43qfs..",{
+const aefnet_LoginSystem = new Sequelize("aefnet_AccountSystem","aefnet_AccountSystem","*x-#~%vr^H5P",{
     dialect: "mysql",
-    host: "aefnet.com"
+    host: "aefnet.com",
+    define: { timestamps: false }
 })
 
 module.exports = aefnet_LoginSystem;
--- utility/aefnet_sayac.js
+++ utility/aefnet_sayac.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const aefnet_sayac = new Sequelize("aefnet_sayac","aefnet_sayac","LdVhKf1=oZe3",{
     dialect: "mysql",
-    host: "aefnet.com"
+    host: "aefnet.com",
+    define: { timestamps: false }
 })
 
 module.exports = aefnet_sayac;
